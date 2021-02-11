5a809169c63ae7eea5ba6d38622d98f837aee55c
Lukas <fischi.ftw@gmail.com>
2021-02-11 20:44:46 +0100

    Publishing v1.1.0

--- component---src-pages-404-tsx-fbb29e883b59ba8490d8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),r=a("Ztgr"),s=a("soUV"),u=a("1Yd/");t.default=function(){return l.a.createElement(s.a,null,l.a.createElement(u.a,{title:"404 Not found"}),l.a.createElement(r.a,null),l.a.createElement("div",{className:"container mt-3"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-fbb29e883b59ba8490d8.js.map
